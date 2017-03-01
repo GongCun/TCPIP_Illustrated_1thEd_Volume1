a64fce59ba20c89886132eeb55d423b5f389cd28
GongCun <Gong Cun>
2017-03-02 06:18:09 +0800

    stop-and-wait

--- RDT/rdt_recv.c
+++ RDT/rdt_recv.c
@@ -7,11 +7,13 @@ ssize_t rdt_recv(void *buf, size_t nbyte)
 	static int seq = 0;
 	struct rdthdr *rptr;
 	struct conn_user *cptr;
+
 	cptr = &conn_user;
 
 	n = read(cptr->pfd, cptr->pkt, cptr->mss);
 	rptr = (struct rdthdr *)cptr->pkt;
 
+        fprintf(stderr, "> rdt_recv() expect seq: %d\n", seq);
         fprintf(stderr, "> recv pkt:\n");
         pkt_debug(rptr);
 
@@ -20,7 +22,7 @@ ssize_t rdt_recv(void *buf, size_t nbyte)
 	{
 		/* Send _NoAck_ packet */
 		n = make_pkt(cptr->src, cptr->dst, cptr->scid, cptr->dcid,
-                        seq, RDT_ACK, NULL, 0, cptr->pkt);
+                        rptr->rdt_seq, RDT_ACK, NULL, 0, cptr->pkt);
 		if ((n = to_net(cptr->sfd, cptr->pkt, n, cptr->dst)) < 0)
 			return(n);
 
